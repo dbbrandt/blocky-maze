cdf44e63aeffda899e637ae40382d3452eeee032
Daniel Brandt <dbbrandt@gmail.com>
2025-08-25 20:53:39 -0400

    Add all block to level one and leave level > 2 with original limited blocks.

--- server/html/maze/src/html.js
+++ server/html/maze/src/html.js
@@ -72,11 +72,19 @@ ${Maze.html.helpDialogs_()}
  */
 Maze.html.toolbox_ = function(level) {
   let xml = `
-<block type="maze_moveForward"></block>
-<block type="maze_turn"><field name="DIR">turnLeft</field></block>
-<block type="maze_turn"><field name="DIR">turnRight</field></block>
+      <block type="maze_moveForward"></block>
+      <block type="maze_turn"><field name="DIR">turnLeft</field></block>
+      <block type="maze_turn"><field name="DIR">turnRight</field></block>
+      <block type="maze_forever"></block>
+      <block type="maze_if"></block>
+      <block type="maze_ifElse"></block>
 `;
   if (level > 2) {
+    xml = `
+    <block type="maze_moveForward"></block>
+    <block type="maze_turn"><field name="DIR">turnLeft</field></block>
+    <block type="maze_turn"><field name="DIR">turnRight</field></block>
+    `;
     xml += '<block type="maze_forever"></block>\n';
     if (level === 6) {
       xml += '<block type="maze_if"><field name="DIR">isPathLeft</field></block>\n';
